60877307be0ab554bc7e10dce4b5402925cb2b95
Jakub Leska <jakub@leska.me>
2026-05-28 16:29:22 +0200

    Release Connect beta60.

--- connect/android/app/build.gradle.kts
+++ connect/android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.ohd.connect"
         minSdk = 29
         targetSdk = 34
-        versionCode = 59
-        versionName = "0.1.0-beta59"
+        versionCode = 60
+        versionName = "0.1.0-beta60"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         // The cdylibs Stage 1 (cargo-ndk) drops into
